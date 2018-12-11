a20d863c7e4701afca047686df7b4883f9474762
srijeeta-bera28 <43879477+srijeeta-bera28@users.noreply.github.com>
2018-12-11 11:27:07 +0530

    try1

---
+++ js/testjs.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
